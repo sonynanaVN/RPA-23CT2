05e3b76216b2e038ee8ffcc745c3885e9c2e19b6
sony <168882866+sonynanaVN@users.noreply.github.com>
2026-03-24 18:48:40 +0700

    thai ngu

--- BTbuoi7-beautifulsoup-FollowTeacher.py
+++ BTbuoi7-beautifulsoup-FollowTeacher.py
@@ -1,7 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-ULR="https://www.thegioididong.com/dtdd"
-response = requests.get(ULR)
-text = response.text
-BeautifulSoup()
+URL = "https://books.toscrape.com/"
+response = requests.get(URL)
+text_response = response.content.decode("utf-8")
+print(text_response)
+soup = BeautifulSoup(text_response, "html.parser")
+
+book_product = soup.find_all("article", class_="product_pod")
+for i_book in book_product:
+    print(i_book)
+    name = i_book.h3.a["title"]
+print(name)
