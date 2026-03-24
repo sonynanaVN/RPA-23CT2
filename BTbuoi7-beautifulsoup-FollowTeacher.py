08e83b2f2300f9cff4343af21c1363afee8c6808
sony <168882866+sonynanaVN@users.noreply.github.com>
2026-03-24 19:27:42 +0700

    Done 7

--- BTbuoi7-beautifulsoup-FollowTeacher.py
+++ BTbuoi7-beautifulsoup-FollowTeacher.py
@@ -1,14 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
-
+import pandas as pd
 URL = "https://books.toscrape.com/"
 response = requests.get(URL)
-text_response = response.content.decode("utf-8")
-print(text_response)
-soup = BeautifulSoup(text_response, "html.parser")
+
+soup = BeautifulSoup(response.text, "html.parser")
 
 book_product = soup.find_all("article", class_="product_pod")
-for i_book in book_product:
-    print(i_book)
-    name = i_book.h3.a["title"]
-print(name)
+data = []
+for book in book_product:
+    name = book.h3.a["title"]
+    print(f"Tên sách: {name}")
+    price = book.find("p", class_="price_color").text
+    print(f"Giá: {price}")
+    in_stock = book.find("p", class_="instock availability").text.strip()
+    print(f"Tình trạng: {in_stock}")
+    startting = book.find("p", class_="star-rating")["class"][1]
+    print(f"Đánh giá: {startting} starts")
+    print("-" * 30)
+data.append({
+    "Tên sách": name,
+    "Giá": price,
+    "Tình trạng": in_stock,
+    "Đánh giá": startting
+})
+df = pd.DataFrame(data)
+df.to_excel("Pandas.xlsx", index=False,engine="openpyxl")
+print("Dữ liệu đã được lưu vào Pandas.xlsx")
+#homework : Lấy dữ liệu từ 10 trang đầu tiên của website và lưu vào Excel, sau đó thống kê số lượng sách theo từng đánh giá (1-5 sao) và giá trung bình của mỗi loại đánh giá.
